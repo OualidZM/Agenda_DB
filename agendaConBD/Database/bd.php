6ceb9449d0dcbbd345aa181c04a993490b284e74
OualidZM <ozaaj@cifpfbmoll.eu>
2021-12-14 13:17:25 +0100

    docs: :page_facing_up: DB
    Changed PDO from mysql to postgres

--- agendaConBD/Database/bd.php
+++ agendaConBD/Database/bd.php
@@ -5,15 +5,16 @@ class Databasee{
 
     
     private $host = "localhost";
-    private $user  = "root";
-    private $dbpass = "mysqlroot";
+    private $user  = "postgres";
+    private $dbpass = "postgrespg";
+    private $dbname = "agendadb";
     public $conn;
     
     public function connection(){
         $this->conn = null;
 
         try {
-            $this->conn = new PDO("mysql:host" . $this->host, $this->user,$this->dbpass);
+            $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->dbname, $this->user,$this->dbpass);
 
         }catch(PDOException $error) {
             echo "Can't connect: " . $error->getMessage();
